ce0f8720b4333ab9ed47d1fcc09ffaf5d052621e
ChaosCypher <jamie@chaoscypher.ca>
2021-05-23 17:08:47 -0400

    initial

---
+++ bidir-traffic-check.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# add path to tcpdump (if empty defaults to $PATH)
+TCPDUMP_PATH
+
+# set network interfaces to check (if empty defaults to all interfaces)
+INTERFACES=
+
+# set return codes
+OK=0
+ERROR=1
+WARNING=2
+UNKNOWN=3
+
+function checkRoot () {
+}
+
+function getInterfaces () {
+}
+
+function checkTraffic () {
+}
+
+function main () {
+}
